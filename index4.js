347f524436b18bba6abc346a18e49e2fc9787ff0
King Etiosasere <iyosa14@gmail.com>
2021-11-18 05:47:30 +0100

    trying this out

---
+++ index4.js
@@ -0,0 +1,7 @@
+function me() {
+	console.log(this)
+	console.log(this.name)
+	console.log("something is moving, something is changing, see his glory")
+  console.log('i am checking something out againnnnnn')
+	return this
+}
